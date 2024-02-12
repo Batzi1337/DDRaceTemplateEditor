6c3a5b0e509f22d167a3800941115433086b40e6
Batzi1337 <28926863+Batzi1337@users.noreply.github.com>
2024-02-12 14:06:16 +0100

    refactor code in order to increase performance

---
+++ assets_test.go
@@ -0,0 +1,99 @@
+package assets
+
+import (
+	"image"
+	"image/draw"
+	"image/png"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestReplaceItems(t *testing.T) {
+	srcImage := getImage("../../samples/templates/purple_haze.png")
+	dstImage := getImage("../../samples/templates/template1.png")
+
+	src := NewTemplate(srcImage)
+	dst := NewTemplate(dstImage)
+	replacement := src.NewItem(64, 32, 128, 96)
+
+	dst.ReplaceItems(replacement)
+
+	// Verify that the replaced item is present in the result
+	assert.True(t, isItemPresent(*src, *replacement))
+
+	file, err := os.Create("test.png")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	png.Encode(file, dst.Image())
+}
+
+func isItemPresent(src Template, item Item) bool {
+	// Convert the images to RGBA for pixel-level comparison
+	srcRGBA := convertToRGBA(src.Image())
+	itemRGBA := convertToRGBA(item.img)
+
+	// Get the dimensions of the item
+	itemBounds := itemRGBA.Bounds()
+
+	// Iterate over the pixels of the source image
+	for y := srcRGBA.Bounds().Min.Y; y < srcRGBA.Bounds().Max.Y; y++ {
+		for x := srcRGBA.Bounds().Min.X; x < srcRGBA.Bounds().Max.X; x++ {
+			// Check if the current pixel matches the item
+			if matchItem(srcRGBA, itemRGBA, x, y, itemBounds) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func matchItem(src, item *image.RGBA, x, y int, itemBounds image.Rectangle) bool {
+	// Iterate over the pixels of the item
+	for itemY := itemBounds.Min.Y; itemY < itemBounds.Max.Y; itemY++ {
+		for itemX := itemBounds.Min.X; itemX < itemBounds.Max.X; itemX++ {
+			// Get the corresponding pixel in the source image
+			srcPixel := src.RGBAAt(x+itemX, y+itemY)
+			// Get the corresponding pixel in the item image
+			itemPixel := item.RGBAAt(itemX, itemY)
+
+			// Compare the RGB values of the pixels
+			if srcPixel.R != itemPixel.R || srcPixel.G != itemPixel.G || srcPixel.B != itemPixel.B {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func convertToRGBA(img image.Image) *image.RGBA {
+	// Create a new RGBA image with the same bounds as the input image
+	bounds := img.Bounds()
+	rgba := image.NewRGBA(bounds)
+
+	// Draw the input image onto the RGBA image
+	draw.Draw(rgba, bounds, img, bounds.Min, draw.Src)
+
+	return rgba
+}
+
+func getImage(filepath string) image.Image {
+	file, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	img, err := png.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	return img
+}
